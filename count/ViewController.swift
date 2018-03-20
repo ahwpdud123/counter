7654f9e0828a8f3f9e5aa6ed1c8fb4f4b65f020f
D7703_29 <d7703_29@d7703-29ui-imac.local>
2018-03-20 16:03:22 +0900

    adasd

--- count/ViewController.swift
+++ count/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count = 0
+    var value = 0
+    var color = 0
+    @IBOutlet var text: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func tap(_ sender: Any) {
+        count = count + 1
+        text.text = "\(count)"
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    @IBAction func RE(_ sender: Any) {
+        count = 0
+         text.text = "\(count)"
+    }
+    
+    @IBAction func sss(_ sender: Any) {
+        if (color<1) {
+            view.backgroundColor = UIColor.yellow
+            color = color + 1
+        }else{
+            view.backgroundColor = UIColor.cyan
+            color = color - 1
+        }
+    }
+    
 }
 
